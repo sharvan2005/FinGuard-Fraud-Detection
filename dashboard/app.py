9cec27740a9b40805781d1584461beeb1bf07606
sharvan2005 <sharvanadithya@gmail.com>
2026-07-01 18:35:30 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+from sklearn.preprocessing import RobustScaler
 
 # Load trained model
 model = joblib.load("fraud_model.pkl")
@@ -8,14 +9,31 @@ model = joblib.load("fraud_model.pkl")
 st.title("FinGuard Fraud Detection Dashboard")
 st.subheader("Real-Time Credit Card Fraud Prediction")
 
-# File uploader
 uploaded_file = st.file_uploader("Upload transaction CSV", type=["csv"])
 
 if uploaded_file:
     data = pd.read_csv(uploaded_file)
 
     st.write("Uploaded Data:")
-    st.dataframe(data)
+    st.dataframe(data.head())
+
+    # Remove Class column if present
+    if 'Class' in data.columns:
+        data = data.drop('Class', axis=1)
+
+    # Scale Time and Amount exactly like training
+    scaler = RobustScaler()
+
+    data['scaled_amount'] = scaler.fit_transform(
+        data['Amount'].values.reshape(-1, 1)
+    )
+
+    data['scaled_time'] = scaler.fit_transform(
+        data['Time'].values.reshape(-1, 1)
+    )
+
+    # Drop original columns
+    data.drop(['Amount', 'Time'], axis=1, inplace=True)
 
     if st.button("Predict Fraud"):
         predictions = model.predict(data)
